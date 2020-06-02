45f8ab72aeb2f61a60b127bd14966f7dc56f51fc
Simon Planinschek <planinscheksimon@outlook.com>
2020-06-02 18:55:28 +0200

    Add typescript entry

---
+++ src/ts/main.ts
@@ -0,0 +1,24 @@
+import '../css/style.css';
+
+import './calc';
+
+// @ts-ignore
+const go = new Go();
+
+if('instantiateStreaming' in WebAssembly) {
+    WebAssembly.instantiateStreaming(fetch('lib.wasm'), go.importObject).then(r => {
+        go.run(r.instance);
+    })
+        .catch(err => console.error(err));
+}
+else {
+    fetch('lib.wasm')
+    .then(r => {
+        r.arrayBuffer();
+    }).then(byte => {
+        // @ts-ignore
+        WebAssembly.instantiate(byte, go.importObject).then(obj => {
+            go.run(obj.instance);
+        })
+    })
+}
